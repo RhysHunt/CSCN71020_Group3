a88efa8105c8b90b0fd140d0ec21f5a2452f146f
ben <blippert6217@conestogac.on.ca>
2023-11-11 11:26:28 -0500

    Moved rectangle struct to rectangle.h

--- PolygonChecker/Rectangle.h
+++ PolygonChecker/Rectangle.h
@@ -1,2 +1,9 @@
 #pragma once
 void printRectangleArea()
+
+typedef struct points {
+	int x;
+	int y;
+}POINTS;
+
+void getRectSides(POINTS(points[]));
--- PolygonChecker/main.h
+++ PolygonChecker/main.h
@@ -4,10 +4,3 @@ void printWelcome();
 int printShapeMenu();
 double* getTriangleSides(double* triangleSides);
 
-
-typedef struct points {
-	int x;
-	int y;
-}POINTS;
-
-void getRectSides(POINTS(points[]));
